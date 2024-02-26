523ee8aece9d1ef30c166a19c84a0033a47e9c9e
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-02-27 07:11:37 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,7 +1,15 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = float(input("温度を入力してください: "))
+    unit = input("温度の単位（c or f）を入力してください: ") # c or f
+
+    if (unit == "f"):
+        "f to c"
+        temp = (temp-32.0)*5.0/9.0
+    else:
+        "c to f"
+        temp = (temp * 9.0/5.0)+32.0
 
     return temp
 
