9f0925d1c3d2569b90cf96a8c48191c38e199eb2
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-03-01 22:58:22 +0900

    Submit challenge for grading

--- 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
+++ 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
@@ -6,8 +6,18 @@ def ls_command(directory):
     # 無効なディレクトリパスについて考える必要はありません
 
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    files = os.listdir(directory)
+    extensions = [os.path.splitext(f)[1] for f in files if os.path.splitext(f)[1]!=""]
+    dict_extension = {k: extensions.count(k) for k in set(extensions)}
+    extension_sorted = sorted(dict_extension.items(), key=lambda x:x[0])
 
+
+    print("Summary in alphabetical order:")
+    for k, v in extension_sorted:
+        if v==1:
+            print(k+":"+str(v)+" file")
+        else:
+            print(k+":"+str(v)+" files")
 
 if __name__ == "__main__":
     directory_path = input("Enter directory path to organize files: ")
