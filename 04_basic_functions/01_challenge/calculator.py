19fb5821f9a5a5a33f21b57c1b3f78298d3754d2
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-03-02 19:34:52 +0900

    Submit challenge for grading

--- 04_basic_functions/01_challenge/calculator.py
+++ 04_basic_functions/01_challenge/calculator.py
@@ -1,14 +1,31 @@
-def add(a, b):
-    return 0
+def check_decimal(a, b):
+    return (isinstance(a, int) or isinstance(a, float)) and (isinstance(b, int) or isinstance(b, float)) 
 
+def add(a, b):
+    if check_decimal(a, b):
+        return a + b
+    else:
+        return "bad input"
 
 def subtract(a, b):
-    return 0
+    if check_decimal(a, b):
+        return a - b
+    else:
+        return "bad input"
 
 
 def multiply(a, b):
-    return 0
+    if check_decimal(a, b):
+        return a * b
+    else:
+        return "bad input"
 
 
 def divide(a, b):
-    return 0
+    if check_decimal(a, b):
+        if b==0:
+            return "Cannot divide by zero"
+        else:
+            return a / b
+    else:
+        return "bad input"
