07d0f8b0cdf2ed61be0a9c6d646ea18d1dd8c2db
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-03-02 19:42:21 +0900

    Submit challenge for grading

--- 04_basic_functions/03_challenge/main.py
+++ 04_basic_functions/03_challenge/main.py
@@ -1,7 +1,13 @@
 def unique_substrings(input_string):
     # 「pass」を削除して、ここにコードを書いてください
-    pass
-
+    result_list = []
+    for i in range(len(input_string)):
+        for j in range(i+1, len(input_string)+1):
+            if input_string[i:j] in result_list:
+                pass
+            else:
+                result_list.append(input_string[i:j])
+    return result_list
 
 input_string = "banana"
 print(unique_substrings(input_string))
