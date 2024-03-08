05428a1ccbb2ac2f0203d3b801feac29b0c836b0
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-03-08 14:12:28 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,10 +1,13 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
+    number = input("正の整数をインプットしてください: ")
+    number = int(number)
 
     # result変数を編集し、最終的な計算結果を保存します
-    result = 0
+    result = 1
+    for i in range(number):
+        result *= (i + 1)
 
     return result
 
